5c1f5cd9bec55c3df51551f7ff9ef6e1777703b9
akshayks3 <akshayks3573@gmail.com>
2021-05-09 16:59:06 +0530

    use context changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
     if (action.type === 'USER_INPUT') {
@@ -32,9 +33,10 @@ const Login = (props) => {
 
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: undefined })
 
+    const authCtx = useContext(AuthContext)
+
     const { isValid: emailIsValid } = emailState
     const { isValid: passwordIsValid } = passwordState
-
 
     useEffect(() => {
         const identifier = setTimeout(() => {
@@ -65,7 +67,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
